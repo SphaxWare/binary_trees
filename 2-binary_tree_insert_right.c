65c767efb1c792b295ebc1e7e677f682adbe8c95
Sphax <itssphax123@gmail.com>
2024-01-30 11:58:38 -0800

    insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_righ - insert node in right
+ * @parent: parent node
+ * @value: int
+ * Return: pointer
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *right;
+
+
+	if (!parent)
+		return (NULL);
+	right = binary_tree_node(parent, value);
+	if (!right)
+		return (NULL);
+	if (parent->right)
+	{
+		right->right = parent->right;
+		parent->right->parent = right;
+	}
+	parent->right = right;
+	return (right);
+}
