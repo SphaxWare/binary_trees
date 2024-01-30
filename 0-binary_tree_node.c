770b55a248e641970911b44dbab9b43050bac48c
Sphax <itssphax123@gmail.com>
2024-01-30 11:22:21 -0800

    create a node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: parent node
+ * @value: int
+ * Return: pointer
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	node->parent = parent;
+	node->n = value;
+	return (node);
+}
