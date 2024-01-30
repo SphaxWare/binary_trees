150ffd622e73f6e69f939f14cde39217356556b2
Sphax <itssphax123@gmail.com>
2024-01-30 11:42:30 -0800

    Insertleft

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert node in left
+ * @parent: parent node
+ * @value: int
+ * Return: pointer
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *left;
+
+
+	left = malloc(sizeof(binary_tree_t));
+	if (left == NULL || parent == NULL)
+		return (NULL);
+	left->n = value;
+	left->left = NULL;
+	left->right = NULL;
+	if (parent->left)
+        {
+                left->left = parent->left;
+                parent->left->parent = left;
+        }
+        else
+                parent->left = left;
+	return (left);
+}
