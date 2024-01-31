e3e75707742a7e6ad4bc3960c6a4badd30070c4c
Sphax <itssphax123@gmail.com>
2024-01-31 11:25:31 -0800

    height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - height of a binary tree
+ * @tree: the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+
+	if (!tree)
+		return (0);
+	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (left > right ? left : right);
+}
