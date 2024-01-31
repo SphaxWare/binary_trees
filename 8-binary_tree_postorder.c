0c3cd8224768c93bdec27c571b3da0bb79f7f433
Sphax <itssphax123@gmail.com>
2024-01-31 10:29:38 -0800

    fix betty

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree using post-order
+ * traversal
  * @tree: the tree
  * @func: the function
  */
